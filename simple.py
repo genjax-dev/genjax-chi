925f0d14d770210a28b51d10494568cbfef17070
McCoy Becker <mccoybecker@gmail.com>
2022-08-08 12:25:00 -0700

    Added initial zero cost effect handler interpreter.

--- simple.py
+++ simple.py
@@ -12,13 +12,19 @@ def f(key, x):
     return (key, 2 * (m1 + m2))
 
 
+# Initialize a PRNG.
 key = jax.random.PRNGKey(314159)
+
+# This just shows our raw (not yet desugared/codegen) syntax.
 expr = gex.lift(f, key, 0.3)
 print(expr)
+
+# Here's how you access the `simulate` GFI.
 fn = gex.Simulate().jit(f)(key, 0.3)
 tr = fn(key, 0.3)
 print(tr.get_choices())
 
+# Here's how you access the `generate` GFI.
 chm = {("m1",): True}
 fn = gex.Generate(chm).jit(f)(key, 0.3)
 w, tr = fn(key, 0.3)
