2c4becb52017135741d4aa56ff68e88384def891
McCoy Becker <mccoybecker@gmail.com>
2022-08-08 12:27:02 -0700

    Added initial zero cost effect handler interpreter.

--- simple.py
+++ simple.py
@@ -5,11 +5,20 @@
 import jax
 import gex
 
+# A `gex` generative function is a pure Python function from
+# `(PRNGKey, *args)` to `(PRNGKey, retval)`
+#
+# The programmer is free to use other JAX primitives, etc -- as desired.
+def g(key, x):
+    key, m1 = gex.trace("m1", gex.Bernoulli)(key, x)
+    return (key, m1)
+
 
 def f(key, x):
     key, m1 = gex.trace("m1", gex.Bernoulli)(key, x)
     key, m2 = gex.trace("m2", gex.Bernoulli)(key, x)
-    return (key, 2 * (m1 + m2))
+    key, m3 = gex.trace("m3", g)(key, x)  # We support hierarchical models.
+    return (key, 2 * (m1 + m2 + m3))
 
 
 # Initialize a PRNG.
