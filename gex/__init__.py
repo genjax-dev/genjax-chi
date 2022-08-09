4463faf814382e424285644875ebb0bd9f43c3b0
McCoy Becker <mccoybecker@gmail.com>
2022-08-08 23:32:03 -0700

    Added initial zero cost effect handler interpreter.

--- gex/__init__.py
+++ gex/__init__.py
@@ -12,6 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+__version__ = "0.0.1"
+
 from .core import *
 from .intrinsics import *
 from .handlers import *
---
+++ setup.py
@@ -0,0 +1,58 @@
+# Copyright 2022 MIT ProbComp
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+import re
+
+from setuptools import setup
+
+# Specify the requirements.
+requirements = {
+    "gex": [
+        "jax==0.3.13",
+    ],
+}
+requirements["all"] = [r for v in requirements.values() for r in v]
+
+# Determine the version (hardcoded).
+dirname = os.path.dirname(os.path.realpath(__file__))
+vre = re.compile('__version__ = "(.*?)"')
+m = open(os.path.join(dirname, "gex", "__init__.py")).read()
+__version__ = vre.findall(m)[0]
+
+setup(
+    name="gex",
+    version=__version__,
+    description="Gen ⊗ JAX",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/probcomp/gex",
+    license="Apache-2.0",
+    maintainer="McCoy R. Becker",
+    maintainer_email="mccoyb@mit.edu",
+    classifiers=[
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: Apache Software License",
+        "Topic :: Scientific/Engineering :: Mathematics",
+    ],
+    packages=[
+        "gex",
+    ],
+    package_dir={
+        "gex": "gex",
+    },
+    install_requires=requirements["gex"],
+    extras_require=requirements,
+    python_requires=">=3.8",
+)
