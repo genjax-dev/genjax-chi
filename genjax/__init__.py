207781f6b41c7f3d9ef843b9ad1b4ab919bd6b86
McCoy Becker <mccoybecker@gmail.com>
2022-08-11 16:47:02 -0700

    Remove experimental.

--- genjax/__init__.py
+++ genjax/__init__.py
@@ -12,10 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""
-.. include:: ../README.md
-"""
-
 # Public exports.
 from .core import *
 from .intrinsics import *
