464eeb1d6c3ea4f13375ee2d714ea27862a88a99
McCoy R. Becker <mccoybecker@gmail.com>
2022-08-27 21:19:41 -0400

    Added JAX 2 TF example.

---
+++ examples/genjax2wasm/genjax2wasm.py
@@ -0,0 +1,41 @@
+# Copyright 2022 MIT Probabilistic Computing Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import jax
+from jax.experimental import jax2tf
+import tensorflow as tf
+import genjax
+
+@genjax.gen
+def model(key):
+    x = genjax.trace("x", genjax.Normal)(key, ())
+    return x
+
+def __inner(key, args):
+    key, tr = genjax.simulate(model)(key, args)
+    return key, tr
+
+key = jax.random.PRNGKey(314159)
+f_tf = jax2tf.convert(__inner)
+key, tr = f_tf(key, ())
+print(tr)
+
+fn = tf.function(f_tf, autograph=False, jit_compile=True)
+key, tr = fn(key, ())
+
+#tf.saved_model.save(my_model, '.',
+#        options=tf.saved_model.SaveOptions(experimental_custom_gradients=True)
+#    )
+#
+#restored_model = tf.saved_model.load('.')
