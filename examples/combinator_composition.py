29b9ce2ab1b2c6108190b7ac0ab3f8be79248f17
McCoy Becker <mccoybecker@gmail.com>
2022-09-19 15:10:23 -0400

    Added specialization boundaries to gen_fn APIs in combinators.

--- examples/combinator_composition.py
+++ examples/combinator_composition.py
@@ -21,21 +21,21 @@ import genjax
 # larger patterns of generative computation.
 
 
-@genjax.gen(genjax.Map, in_axes=(0, 0))
-def model1(key, x):
+@genjax.gen(genjax.Map, in_axes=(0,))
+def model1(key):
     key, x = genjax.trace("x", genjax.Normal)(key, (0.0, 1.0))
     key, q = genjax.trace("y", genjax.Bernoulli)(key, (0.3,))
     return (key,)
 
 
-@genjax.gen(genjax.Map, in_axes=(0, 0))
-def model2(key, x):
+@genjax.gen(genjax.Map, in_axes=(0,))
+def model2(key):
     key, x = genjax.trace("y", genjax.Bernoulli)(key, (0.3,))
     return (key,)
 
 
-@genjax.gen(genjax.Map, in_axes=(0, 0))
-def model3(key, x):
+@genjax.gen(genjax.Map, in_axes=(0,))
+def model3(key):
     key, x = genjax.trace("z", genjax.Normal)(key, (0.5, 1.0))
     key, y = genjax.trace("m", genjax.Normal)(key, (0.3, 2.0))
     return (key,)
@@ -48,7 +48,7 @@ def fn():
     key = jax.random.PRNGKey(314159)
     key, *sub_keys = jax.random.split(key, 6)
     sub_keys = jnp.array(sub_keys)
-    _, tr = genjax.simulate(sw)(sub_keys, (1, np.ones(5)))
+    _, tr = genjax.simulate(sw)(sub_keys, (1,))
     key, *sub_keys = jax.random.split(key, 6)
     sub_keys = jnp.array(sub_keys)
     chm = genjax.VectorChoiceMap.new(
@@ -59,7 +59,7 @@ def fn():
         sub_keys,
         tr,
         chm,
-        (2, np.ones(5)),
+        (2,),
     )
     return w, new
 
