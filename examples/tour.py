731433bbdc23ab3a400ef415f06944fff24ceb93
McCoy Becker <mccoybecker@gmail.com>
2022-11-23 14:15:51 -0500

    Add incremental propagation interpreter for update.

--- examples/tour.py
+++ examples/tour.py
@@ -21,67 +21,34 @@ import genjax
 # pretty printing of `Pytree` objects.
 console = genjax.go_pretty()
 
-# A `genjax` generative function is a pure Python function from
-# `(PRNGKey, *args)` to `(PRNGKey, retval)`
-#
-# The programmer is free to use other JAX primitives, etc -- as desired.
-#
-# The models below are rather simplistic, but demonstrate the basics.
-
-
-@genjax.gen
-def h(key, x):
-    key, m1 = genjax.trace("m0", genjax.Bernoulli)(key, (x,))
-    return (key, m1)
-
-
-@genjax.gen
-def g(key, x):
-    key, m1 = genjax.trace("m0", h)(key, (x,))
-    return (key, m1)
-
 
 @genjax.gen
 def f(key, x):
     key, m0 = genjax.trace("m0", genjax.Bernoulli, shape=(3, 3))(key, (x,))
-    key, m4 = genjax.trace("m4", g)(key, (x,))
-    key, m5 = genjax.trace("m5", genjax.Normal)(key, (0.0, 1.0))
-    return key, (2 * m0 * m4, m5)
+    key, m1 = genjax.trace("m1", genjax.Normal, shape=(3, 3))(key, (0.0, 1.0))
+    return key, m1 * m0
 
 
 # Initialize a PRNG.
 key = jax.random.PRNGKey(314159)
+jaxpr = jax.make_jaxpr(f)(key, 0.3)
 
-# This just shows our raw (not yet desugared/codegen) syntax.
-expr = jax.make_jaxpr(f)(key, 0.3)
-print(expr)
-
-# Here's how you access the `simulate` GFI.
 key, tr = jax.jit(genjax.simulate(f))(key, (0.3,))
+console.print(tr.get_retval())
 
-# Here's how you access the `importance` GFI.
-chm = genjax.ChoiceMap.new({("m0",): True, ("m4", "m0", "m0"): False})
-key, (w, tr) = jax.jit(genjax.importance(f))(key, chm, (0.3,))
-assert tr[("m4", "m0", "m0")] == False
+maker = jax.make_jaxpr(genjax.update(f))
+jaxpr = maker(
+    key,
+    tr,
+    genjax.EmptyChoiceMap(),
+    (genjax.Diff.new(0.3, genjax.NoChange),),
+)
+print(jaxpr)
 
-# Here's how you access the `update` GFI.
-jitted = jax.jit(genjax.update(f))
-
-chm = genjax.ChoiceMap.new({("m0",): False})
-key, (w, updated, discard) = jitted(key, tr, chm, (0.3,))
-
-chm = genjax.ChoiceMap.new({("m4", "m0", "m0"): True})
-console.print(chm)
-key, (w, updated, discard) = jitted(key, tr, chm, (0.3,))
-assert updated[("m4", "m0", "m0")] == True
-
-# Here's how you access the `arg_grad` GFI.
-jitted = jax.jit(genjax.arg_grad(f, argnums=1))
-key, grad = jitted(key, tr, (0.3,))
-console.print(grad)
-
-# Here's how you access the `choice_grad` GFI.
-jitted = jax.jit(genjax.choice_grad(f))
-selected = genjax.Selection(["m5"])
-key, choice_grad = jitted(key, tr, selected)
-console.print(choice_grad["m5"])
+jaxpr = maker(
+    key,
+    tr,
+    genjax.ChoiceMap.new({("m0",): True}),
+    (genjax.Diff.new(0.3, genjax.NoChange),),
+)
+print(jaxpr)
