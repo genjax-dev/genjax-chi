302c7e99983395c4cca913c662e1abd75629958b
McCoy Becker <mccoybecker@gmail.com>
2022-12-02 09:36:30 -0500

    Added assess to all combinators. Filling in more tests. Filling in more docs.

--- docs/jupyter_execute/genjax/gen_fn.py
+++ docs/jupyter_execute/genjax/gen_fn.py
@@ -5,12 +5,15 @@
 
 
 import jax
+
 import genjax
+
 
 @genjax.gen
 def model(key):
     key, x = genjax.trace("x", genjax.Normal)(key, 0.0, 1.0)
     return key, x
+
 
 print(model)
 
@@ -35,6 +38,7 @@ print(jaxpr.pretty_print(use_color=False))
 
 
 import genjax
+
+
 fn = genjax.simulate(genjax.Normal)
 print(fn)
-
