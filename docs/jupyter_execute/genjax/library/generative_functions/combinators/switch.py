de0aac15f68e185ae7524b0289c54a4c3ec3d89a
McCoy Becker <mccoybecker@gmail.com>
2022-12-23 22:21:34 -0800

    Setup public API.

--- docs/jupyter_execute/genjax/library/generative_functions/combinators/switch.py
+++ docs/jupyter_execute/genjax/library/generative_functions/combinators/switch.py
@@ -5,23 +5,26 @@
 
 
 import jax
+
 import genjax
+
 
 @genjax.gen
 def branch_1(key):
     key, x = genjax.trace("x1", genjax.Normal)(key, 0.0, 1.0)
-    return (key, )
+    return (key,)
+
 
 @genjax.gen
 def branch_2(key):
     key, x = genjax.trace("x2", genjax.Bernoulli)(key, 0.3)
-    return (key, )
+    return (key,)
+
 
 switch = genjax.SwitchCombinator([branch_1, branch_2])
 
 key = jax.random.PRNGKey(314159)
 jitted = jax.jit(genjax.simulate(switch))
-key, _ = jitted(key, (0, ))
-key, tr = jitted(key, (1, ))
+key, _ = jitted(key, (0,))
+key, tr = jitted(key, (1,))
 print(tr)
-
