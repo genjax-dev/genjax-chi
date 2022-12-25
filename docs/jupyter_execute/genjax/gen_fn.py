50ac8af97f5ead2f4fa50dc53ae3ce7a62201f58
McCoy Becker <mccoybecker@gmail.com>
2022-12-24 20:17:24 -0800

    Setup public API.

--- docs/jupyter_execute/genjax/gen_fn.py
+++ docs/jupyter_execute/genjax/gen_fn.py
@@ -8,9 +8,9 @@ import jax
 import genjax
 
 @genjax.gen
-def model(key):
-    key, x = genjax.trace("x", genjax.Normal)(key, 0.0, 1.0)
-    return key, x
+def model():
+    x = genjax.trace("x", genjax.Normal)(0.0, 1.0)
+    return x
 
 print(model)
 
