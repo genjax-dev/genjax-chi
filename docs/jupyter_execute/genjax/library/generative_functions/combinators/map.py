afd971093b1f5c13eec00b7c8460273206c301bc
McCoy Becker <mccoybecker@gmail.com>
2023-01-16 21:45:56 -0500

    Local.

--- docs/jupyter_execute/genjax/library/generative_functions/combinators/map.py
+++ docs/jupyter_execute/genjax/library/generative_functions/combinators/map.py
@@ -6,23 +6,23 @@
 
 import jax
 import jax.numpy as jnp
+
 import genjax
+
+
 console = genjax.pretty()
+
 
 @genjax.gen
 def add_normal_noise(x):
-    noise1 = genjax.trace("noise1", genjax.Normal)(
-            0.0, 1.0
-    )
-    noise2 = genjax.trace("noise2", genjax.Normal)(
-            0.0, 1.0
-    )
+    noise1 = genjax.trace("noise1", genjax.Normal)(0.0, 1.0)
+    noise2 = genjax.trace("noise2", genjax.Normal)(0.0, 1.0)
     return (key, x + noise1 + noise2)
+
 
 mapped = genjax.MapCombinator.new(add_normal_noise, in_axes=(0,))
 
 arr = jnp.ones(100)
 key = jax.random.PRNGKey(314159)
-key, tr = jax.jit(genjax.simulate(mapped))(key, (arr, ))
+key, tr = jax.jit(genjax.simulate(mapped))(key, (arr,))
 console.print(tr)
-
