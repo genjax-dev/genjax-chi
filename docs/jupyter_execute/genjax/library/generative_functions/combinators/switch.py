87681fda093f3057d7ceba6448845beecc7f9031
McCoy Becker <mccoybecker@gmail.com>
2023-01-16 21:45:56 -0500

    Local.

--- docs/jupyter_execute/genjax/library/generative_functions/combinators/switch.py
+++ docs/jupyter_execute/genjax/library/generative_functions/combinators/switch.py
@@ -5,22 +5,27 @@
 
 
 import jax
+
 import genjax
+
+
 console = genjax.pretty()
+
 
 @genjax.gen
 def branch_1():
     x = genjax.trace("x1", genjax.Normal)(0.0, 1.0)
 
+
 @genjax.gen
 def branch_2():
     x = genjax.trace("x2", genjax.Bernoulli)(0.3)
+
 
 switch = genjax.SwitchCombinator([branch_1, branch_2])
 
 key = jax.random.PRNGKey(314159)
 jitted = jax.jit(genjax.simulate(switch))
-key, _ = jitted(key, (0, ))
-key, tr = jitted(key, (1, ))
+key, _ = jitted(key, (0,))
+key, tr = jitted(key, (1,))
 console.print(tr)
-
