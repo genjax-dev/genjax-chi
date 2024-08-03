6f073019c39cab4252c3cdc53e525845b6d18689
McCoy Becker <mccoybecker@gmail.com>
2024-08-02 21:41:41 -0400

    Add back inference tests.

--- src/genjax/_src/core/generative/choice_map.py
+++ src/genjax/_src/core/generative/choice_map.py
@@ -417,7 +417,7 @@ class _ChoiceMapBuilder(Pytree):
             return choice_map_empty
 
     def n(self) -> "EmptyChm":
-        return choice_map_emptforced_y
+        return choice_map_empty
 
     def v(self, v) -> "ValChm":
         return ChoiceMap.value(v)
